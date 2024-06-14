62e4d5d459ca53fba187f8f2f8dc0d5a0319da08
Kai Hilbourne <kaihilbourne@umail.ucsb.edu>
2024-06-14 16:28:33 -0700

    gubs

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 import {JoinGame} from './JoinGame/JoinGame.js'
 
+//
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:4000');
+//
+
+
 function App() {
   return (
     <Router>
--- src/JoinGame/JoinGame.js
+++ src/JoinGame/JoinGame.js
@@ -1,9 +1,41 @@
-import "./JoinGame.css"
+import { useState,useRef } from "react";
+import "./JoinGame.css";
+import io from 'socket.io-client';
 
 export function JoinGame(){
+    const roomRef = useRef(null);
+
+    const [room,setRoom] = useState('');
+    const [usename,setName] = useState('');
+
+    io.on('connection', (socket) => {
+        console.log('a user connected');
+        socket.on('disconnect', () => {
+            console.log('user disconnected');
+        });
+    });
+
+    const joinRoom = (room) => {
+        setRoom(roomRef.current.value);
+        console.log(roomRef.current.value);
+    };
     return (
         <div className="page">
-            I'm mr uggo
+            <div className="middle">
+                Save the Gubs!
+            </div>
+            <input 
+                ref={roomRef}
+                type="text"
+                placeholder="Room name"
+            />
+            <input 
+                type="text" 
+                value={usename}
+                onChange={(e) => setName(e.target.value)}
+                placeholder="your name"
+            />
+            <button onClick={joinRoom}>Enter</button>
         </div>
     );
 }
