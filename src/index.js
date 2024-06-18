4f6f666f663706369c7d4a9ca361dd6afb12cc74
Kai Hilbourne <kaihilbourne@umail.ucsb.edu>
2024-06-17 22:07:52 -0700

    realtime database new solution

--- src/index.js
+++ src/index.js
@@ -4,6 +4,35 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, set } from 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBtYu3SY_XTXzDc3lK09944M_qfdIqJyIA",
+  authDomain: "gubs-online.firebaseapp.com",
+  databaseURL: "https://gubs-online-default-rtdb.firebaseio.com",
+  projectId: "gubs-online",
+  storageBucket: "gubs-online.appspot.com",
+  messagingSenderId: "936911098464",
+  appId: "1:936911098464:web:f762c9ebaa51c723362905",
+  measurementId: "G-2DEX510CWW"
+};
+
+const app = initializeApp(firebaseConfig);
+
+function updateUserScores(userID,uname,gamerecord){
+  const db = getDatabase();
+  const reference = ref(db,"users/"+userID);
+  
+
+
+  set(reference, {
+      username: uname,
+      gamerecord: gamerecord
+  });
+}
+
+updateUserScores("burg","burgsauce","{wins: 1}");
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,7 +41,5 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
