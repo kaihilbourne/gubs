f5508274c0913d3c978cb2979e81f7fe2700755d
Kai Hilbourne <kaihilbourne@umail.ucsb.edu>
2024-06-24 21:23:08 -0700

    New Realtime Database branch

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, set } from 'firebase/database';
